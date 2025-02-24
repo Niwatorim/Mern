a25d7f9306906b81982311f3c56420237f435109
Niwatori <niwatorim001@gmail.com>
2025-02-24 13:59:56 +0400

    Final version, finalized for diployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ dotenv.config();
 //import db connection
 import {connect} from './config/db.js';
 
+//for deployment
+import path from 'path'
+
 //getting the routes from other file (scalability)
 import productRoutes from './routes/product.routes.js';
 
@@ -12,6 +15,14 @@ const app =express(); //Makes express app
 app.use(express.json());//allows app to use json data
 const PORT = process.env.PORT
 
+//
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV ==='production'){
+    app.use(express.static(path.join(__dirname,'frontend/dist')))//path.join basically means just go under the root and go under dist folder 
+    app.get('*', (req,res)=>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist','index.html'))); 
+}
 
 
 app.use('/api/products', productRoutes); // any routes are prefixed with /api/products
